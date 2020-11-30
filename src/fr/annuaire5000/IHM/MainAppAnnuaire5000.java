11027d866a745f7265ccacb752a709c01d0afdb3
alexdelt <alexander.deltour@gmail.com>
2020-11-30 10:05:08 +0100

    changement du nom de la class mainapp en mainapppannuaire5000

--- src/fr/annuaire5000/IHM/MainAppAnnuaire5000.java
+++ src/fr/annuaire5000/IHM/MainAppAnnuaire5000.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class MainApp extends Application {
+public class MainAppAnnuaire5000 extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
