c5f831bdeb89b84fe1beeae4388164a6ddd09ccc
khaled <khaledinat@gmail.com>
2020-12-02 11:43:55 +0100

    Ajouter icon au projet

--- src/fr/annuaire5000/IHM/MainAppAnnuaire5000.java
+++ src/fr/annuaire5000/IHM/MainAppAnnuaire5000.java
@@ -1,7 +1,9 @@
 package fr.annuaire5000.IHM;
 
+
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class MainAppAnnuaire5000 extends Application {
@@ -9,20 +11,18 @@ public class MainAppAnnuaire5000 extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
-		MainPanel root = new MainPanel();
-		
+		MainPanel root = new MainPanel();	
 		Scene scene = new Scene(root, 1400, 800);
 		primaryStage.setScene(scene);
 		primaryStage.setResizable(false);
 		primaryStage.setTitle("Annuaire5000");
+		Image img = new Image("file:book.png");
+		primaryStage.getIcons().add(img);	
 		primaryStage.sizeToScene();
 		primaryStage.show();
 	}
-
 	public static void main(String[] args) {
 		launch(args);
-		
-		
 
 	}
 
